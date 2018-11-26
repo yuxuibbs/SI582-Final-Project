fb9178af55b0b707bc3c8cb04d112097a7fcd006
yuxuanc <yuxuanc@umich.edu>
2018-11-25 23:29:49 -0500

    finish prototype

---
+++ js/charts.js
@@ -0,0 +1,19 @@
+function displayAll() {
+    $("img").attr("src", 'img/all.png');
+}
+
+function firstRange() {
+    $("img").attr("src", 'img/1-12.png');
+}
+
+function secondRange() {
+    $("img").attr("src", 'img/13-24.png');
+}
+
+function thirdRange() {
+    $("img").attr("src", 'img/25-34.png');
+}
+
+function fourthRange() {
+    $("img").attr("src", 'img/35-45.png');
+}
